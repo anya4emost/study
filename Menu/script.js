69a57a943771bdba12bc78cb06d8b3c4c5a53fd2
Anna <kashlatch@gmail.com>
2016-11-04 21:29:16 -0700

    menu with animation

---
+++ Menu/script.js
@@ -0,0 +1,7 @@
+var gamburger = document.querySelector('#gamburger');
+var nav = document.querySelector('nav');
+
+
+gamburger.addEventListener('click', function navAppearance() {
+    nav.classList.toggle('show');
+});
